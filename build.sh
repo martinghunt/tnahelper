2a420565ba85b65483fe68269cbd1bf42b085007
Martin Hunt <martinghunt@users.noreply.github.com>
2024-11-15 10:05:08 +0000

    disable CGO_ENABLED

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ for os in windows linux darwin
 do
     for arch in amd64 arm64
     do
-        GOOS=$os GOARCH=$arch go build -ldflags="-X main.Version=${version}" -o tnahelper_${os}_${arch} main.go
+        CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags="-X main.Version=${version}" -o tnahelper_${os}_${arch} main.go
     done
 done
